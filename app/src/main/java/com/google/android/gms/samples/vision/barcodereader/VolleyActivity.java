02f512f23e28ccd829ba23bf37007fd5e6b8b1f6
GiulioPettenuzzo <giulio.pettenuzzo@studenti.unipd.it>
2017-05-16 10:14:52 +0200

    setting new layout resource file with constraint layout, added two new button without coding

--- app/src/main/java/com/google/android/gms/samples/vision/barcodereader/VolleyActivity.java
+++ app/src/main/java/com/google/android/gms/samples/vision/barcodereader/VolleyActivity.java
@@ -4,8 +4,10 @@ package com.google.android.gms.samples.vision.barcodereader;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -21,11 +23,7 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.vision.barcode.Barcode;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.StringTokenizer;
 
 
 public class VolleyActivity extends AppCompatActivity {
@@ -34,9 +32,18 @@ public class VolleyActivity extends AppCompatActivity {
     ImageView mImageView;
     ImageButton buttonPrev;
     ImageButton buttonNext;
+    ImageButton photoButton;
+    ImageButton imageSelected;
 
     private int numPhotoSelected;
     private int maxNumImage;
+    boolean isClicked = false;
+
+    ImageUrlUnpacker imageUrlUnpacker = new ImageUrlUnpacker();
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+
 
     // constants used to pass extra data in the intent
     public static final String initialYahooURL = "https://it.images.search.yahoo.com/search/images;_ylt=A9mSs3TQLxBZDrgATj0bDQx.;_ylu=X3oDMTB0ZTgxN3Q0BGNvbG8DaXIyBHBvcwMxBHZ0aWQDBHNlYwNwaXZz?p=";
@@ -51,15 +58,22 @@ public class VolleyActivity extends AppCompatActivity {
         mImageView = (ImageView) findViewById(R.id.imageView);
         buttonPrev = (ImageButton) findViewById(R.id.button_prev);
         buttonNext = (ImageButton) findViewById(R.id.button_next);
-        Intent intent = getIntent();
-        Barcode barcode = intent.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
+        photoButton = (ImageButton) findViewById(R.id.photoButton);
+        imageSelected = (ImageButton) findViewById(R.id.saveImageButton);
+
+/**
+        // THE PART OF THE CODE BELOW IS USED FOR SET THE ACTIVITY WITH THE BARCODE READER
+         Intent intent = getIntent();
+         Barcode barcode = intent.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
 
         //recive the intent from BarcodeCaptureActivity who sent the barcode number
         mTextView.setText(barcode.displayValue);
 
-        //String url ="https://it.images.search.yahoo.com/search/images;_ylt=A9mSs3TQLxBZDrgATj0bDQx.;_ylu=X3oDMTB0ZTgxN3Q0BGNvbG8DaXIyBHBvcwMxBHZ0aWQDBHNlYwNwaXZz?p=8001435500013&fr=yfp-t-909&fr2=piv-web";
         final String url = initialYahooURL + barcode.displayValue + finalYahooURL;
-        final ImageUrlUnpacker imageUrlUnpacker = new ImageUrlUnpacker();
+*/
+        String url ="https://it.images.search.yahoo.com/search/images;_ylt=A9mSs3TQLxBZDrgATj0bDQx.;_ylu=X3oDMTB0ZTgxN3Q0BGNvbG8DaXIyBHBvcwMxBHZ0aWQDBHNlYwNwaXZz?p=8001435500013&fr=yfp-t-909&fr2=piv-web";
+        mTextView.setText("8001435500013");
+
 
         RequestQueue queue = Volley.newRequestQueue(this);
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
@@ -69,9 +83,14 @@ public class VolleyActivity extends AppCompatActivity {
                         //saving response for pircing
                         imageUrlUnpacker.setResponce(response);
                         String image_url = imageUrlUnpacker.getMyString();
-                        new DownloadImageTask((ImageView) mImageView)
-                                .execute(image_url);
-                        maxNumImage = imageUrlUnpacker.countImageOnUrl();
+                        if(image_url.compareTo("error")!=0) {
+                            new DownloadImageTask((ImageView) mImageView)
+                                    .execute(image_url);
+                            maxNumImage = imageUrlUnpacker.countImageOnUrl();
+                        }
+                        else{
+                            //TODO: open a dialog in order to ask at user if he want to use a camera to take a picture about the element
+                        }
                     }
                 }, new Response.ErrorListener() {
             @Override
@@ -117,6 +136,37 @@ public class VolleyActivity extends AppCompatActivity {
                         .execute(image_url);
             }
         });
+
+        final Drawable drawable_saved = getDrawable(R.drawable.ic_check_green_24dp);
+        final Drawable drawable_not_saved = imageSelected.getDrawable();
+        imageSelected.setOnClickListener(new View.OnClickListener() {
+            //change the drawable when the button is clicked and hide the other button
+            @Override
+            public void onClick(View v) {
+               if(isClicked==false){
+                   imageSelected.setImageDrawable(drawable_saved);
+                   buttonPrev.setVisibility(View.GONE);
+                   buttonNext.setVisibility(View.GONE);
+                   photoButton.setVisibility(View.GONE);
+                   isClicked = true;
+               }
+               else{
+                   imageSelected.setImageDrawable(drawable_not_saved);
+                   buttonPrev.setVisibility(View.VISIBLE);
+                   buttonNext.setVisibility(View.VISIBLE);
+                   photoButton.setVisibility(View.VISIBLE);
+                   isClicked = false;
+               }
+            }
+        });
+
+        photoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+        loadAllImage();
     }
 
     /**
@@ -128,6 +178,8 @@ public class VolleyActivity extends AppCompatActivity {
         public DownloadImageTask(ImageView bmImage) {
             this.bmImage = bmImage;
         }
+
+        public DownloadImageTask(){}
 
         protected Bitmap doInBackground(String... urls) {
             String urldisplay = urls[0];
@@ -144,6 +196,36 @@ public class VolleyActivity extends AppCompatActivity {
 
         protected void onPostExecute(Bitmap result) {
             bmImage.setImageBitmap(result);
+        }
+    }
+
+    //maybe it became faster...
+    private void loadAllImage(){
+        for(int current = 1;current<=maxNumImage;current++){
+            imageUrlUnpacker.setImageNum(current);
+            String image_url = imageUrlUnpacker.getMyString();
+            new DownloadImageTask()
+                    .execute(image_url);
+        }
+    }
+
+    //class that send an intent to camera application and recive the extra as the image captured
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+            imageSelected.setImageDrawable(getDrawable(R.drawable.ic_check_green_24dp));
+            buttonPrev.setVisibility(View.GONE);
+            buttonNext.setVisibility(View.GONE);
+            photoButton.setVisibility(View.GONE);
         }
     }
 
